91480b7a166934393acd3ef5f85d317f9f39f568
Aurore Malval <omalval@gmail.com>
2019-08-27 14:50:38 +0200

    did add columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_103615) do
+ActiveRecord::Schema.define(version: 2019_08_27_120604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2019_08_27_103615) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "siret"
+    t.string "profession"
+    t.integer "tax_rate"
+    t.integer "vat"
+    t.string "address"
+    t.string "phone_number"
+    t.string "entreprise_name"
+    t.string "website_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
