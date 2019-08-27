ea393eb4982a84b9978e0b5c80d99f6d7cf68417
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-08-27 15:46:15 +0200

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
   has_many :clients
   has_many :invoices
   has_many :notifications
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true
+  validates :siret, presence: true, uniqueness: true, format: {with: /\A\d{8}\z/, message: "Entrez les 8 chiffres de votre numéro SIRET" }
+  validates :tax_rate, presence: true
 end
