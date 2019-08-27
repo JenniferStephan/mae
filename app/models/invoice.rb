ebf0b125a0860d9045305319b06f554b132f29ab
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-08-27 17:14:11 +0200

    added some validation + enum for the invoice status

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,9 +3,15 @@ class Invoice < ApplicationRecord
   belongs_to :client
   has_many :missions_invoices
 
-STATUS = ["pending", "sent", "paid", "delayed", "archived"]
+enum status: [:pending, :submitted, :sent, :paid, :delayed, :archived]
+
+  before_save :generate_reference if :submitted?
 
   validates :title, presence: true
-  validates :creation_date, presence: true
-  validates :status, presence: true, default: "pending", inclusion: { in: STATUS }
+  # validates :creation_date, presence: true
+  validates :status, presence: true
+
+  def generate_reference
+    binding.pry
+  end
 end
