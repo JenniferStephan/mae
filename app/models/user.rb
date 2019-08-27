b9c1c63a86227582325821012318a9e7f934ca8a
Aurore Malval <omalval@gmail.com>
2019-08-27 15:01:42 +0200

    add table notif

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :missions
+  has_many :clients
+  has_many :invoices
+  has_many :notifications
 end
