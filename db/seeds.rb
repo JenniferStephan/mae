bb5a2ad34f1518555f33b81838e8d62bd16ce8b3
AntoineCartier <antoinecartier.contact@gmail.com>
2019-08-29 00:59:16 +0200

    front advanced

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,32 +39,74 @@ invoice1 = Invoice.new(user: user,
                       reference: "210819-AC0001",
                       creation_date: "21/08/19",
                       due_date: "21/09/19",
-                      total_amount_ttc: "1800")
+                      total_amount_ttc: "1800",
+                      status: 0)
 invoice1.save
 
 invoice2 = Invoice.new(user: user,
-                      title: "Soirée MTV",
+                      title: "Webdesign UX/UI",
                       reference: "140519-AC0002",
                       creation_date: "14/05/19",
                       due_date: "14/06/19",
-                      total_amount_ttc: "4000")
+                      total_amount_ttc: "4000",
+                      status: 4)
 invoice2.save
 
 invoice3 = Invoice.new(user: user,
-                      title: "Mission DDB",
+                      title: "Développement",
                       reference: "050319-AC0003",
                       creation_date: "05/03/19",
                       due_date: "05/03/19",
-                      total_amount_ttc: "3600" )
+                      total_amount_ttc: "3600",
+                      status: 3)
 invoice3.save
 
 invoice4 = Invoice.new(user: user,
-                      title: "Mission Piment",
+                      title: "Front-end",
                       reference: "120119-AC0004",
                       creation_date: "12/01/19",
                       due_date: "12/01/19",
-                      total_amount_ttc: "3100" )
+                      total_amount_ttc: "3100",
+                      status: 3)
 invoice4.save
+
+invoice5 = Invoice.new(user: user,
+                      title: "Mission Consulting",
+                      reference: "120119-AC0004",
+                      creation_date: "12/01/19",
+                      due_date: "12/01/19",
+                      total_amount_ttc: "4300",
+                      status: 5)
+invoice5.save
+
+invoice6 = Invoice.new(user: user,
+                      title: "Webdesign UX/UI",
+                      reference: "120119-AC0004",
+                      creation_date: "12/01/19",
+                      due_date: "12/01/19",
+                      total_amount_ttc: "1700",
+                      status: 5)
+invoice6.save
+
+invoice7 = Invoice.new(user: user,
+                      title: "Mission développement",
+                      reference: "120119-AC0004",
+                      creation_date: "22/10/18",
+                      due_date: "22/11/18",
+                      total_amount_ttc: "2000",
+                      status: 5)
+invoice7.save
+
+invoice8 = Invoice.new(user: user,
+                      title: "Front-end",
+                      reference: "120119-AC0004",
+                      creation_date: "22/10/18",
+                      due_date: "22/11/18",
+                      total_amount_ttc: "2000",
+                      status: 5)
+invoice8.save
+
+
 
 web_design = Mission.create!(name: "web design", description: "doing some website")
 
