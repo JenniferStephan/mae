99840ee9e6ab941994a34f55e1962d4af7c7ba20
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-08-28 21:49:28 +0200

    added the simular on a new pages + created controller action and methods associated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :simulation]
 
   def home
+  end
+
+  def simulation
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  get "/simulate_my_rate", to: "pages#simulation", as: "simulate_my_rate"
+
 
   resources :invoices do
     resources :missions_invoices
@@ -9,6 +11,7 @@ Rails.application.routes.draw do
 
   resources :clients, only: [:show, :new, :create, :edit, :update]
   resources :missions, only: [:new, :create]
+
 
 
 end
