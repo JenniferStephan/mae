23987fbbeeeb1dbf897a59c3686ff0069aecd15f
AntoineCartier <antoinecartier.contact@gmail.com>
2019-08-29 18:46:47 +0200

    invoices index new search bar

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -5,8 +5,8 @@ before_action :set_invoice, only: [:show, :edit, :update, :destroy, :calcul_tota
   def index
     invoices = Invoice.where(user: current_user).order(created_at: :asc)
     if params[:query].present?
-      sql_query = "title ILIKE :query OR reference ILIKE :query OR reference ILIKE :query"
-      @invoices = invoices.where(sql_query, query: "%#{params[:query]}%")
+      sql_query = "invoices.title ILIKE :query OR clients.company_name ILIKE :query OR invoices.reference ILIKE :query"
+      @invoices = invoices.joins(:client).where(sql_query, query: "%#{params[:query]}%")
     else
       @invoices = invoices.all
     end
