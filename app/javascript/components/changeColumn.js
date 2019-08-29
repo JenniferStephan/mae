07a52124f2b48405570311f7363f8d5be5224628
Camille Oleksy <c.oleksy@tbs-education.org>
2019-08-29 17:34:10 +0200

    blabla

---
+++ app/javascript/components/changeColumn.js
@@ -0,0 +1,24 @@
+const changeColumn = () => {
+  const euroButtons = document.querySelectorAll('.checklist-entry i');
+  const paidInvoices = document.querySelector('.invoices-paid');
+  const paidInvoicesList = paidInvoices.querySelector('ul');
+
+  const moveDiv = (event) => {
+    const invoiceDiv = event.target.parentElement.previousElementSibling.querySelector('.invoice-infos');
+    paidInvoicesList.insertAdjacentHTML('beforeend', `
+      <li class="checklist-entry list-group-item flex-column align-items-start">
+        <div class="checklist-item checklist-item-success">
+          <div class="checklist-info">
+            <div class="vertical-line paid"></div>
+            ${invoiceDiv.outerHTML}
+          </div>
+        </div>
+      </li>
+    `);
+    invoiceDiv.parentElement.parentElement.parentElement.remove();
+  }
+
+  euroButtons.forEach(button => button.addEventListener('click', moveDiv))
+}
+
+export default changeColumn;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,12 @@
 import "bootstrap";
 
 import {initNotificationBar} from "../components/notificationBar";
+import changeColumn from '../components/changeColumn';
 
 initNotificationBar();
+changeColumn();
 
+// Enable Bootstrap tooltips
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
