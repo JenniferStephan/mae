5b233a08e28033d1e27700bd577c0cc612cfc802
Aurore Malval <omalval@gmail.com>
2019-09-04 10:00:10 +0200

    chart fixed

--- app/javascript/components/donutChartbis.js
+++ app/javascript/components/donutChartbis.js
@@ -1,34 +1,23 @@
 import Chart from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-
-if (document.querySelector('#doughnut-chart')) {
-
 const total_submitted = document.querySelector('.total_submitted').innerHTML;
 const total_paid = document.querySelector('.total_paid').innerHTML;
 const total_delayed = document.querySelector('.total_delayed').innerHTML;
 
-new Chart(document.getElementById("doughnut-chart"), {
-    type: 'doughnut',
-    data: {
-      labels: ["Factures en attente", "Factures payées", "Factures en retard"],
-      datasets: [
-        {
-          label: "Factures (en euros)",
-          backgroundColor: ["#221ECE", "#64D286","#BE0C62"],
-          data: [total_submitted, total_paid, total_delayed]
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: false,
-        text: 'Mes factures'
-      },
-      legend: {
-            display: true,
-            position: 'right',
-        }
-    }
+const canvas = document.getElementById("myChart");
+const ctx = canvas.getContext('2d');
+const myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Factures en attente", "Factures payées", "Factures en retard"],
+    datasets: [{
+      backgroundColor: [
+        "#221ECE",
+        "#64D286",
+        "#BE0C62"
+      ],
+      data: [total_submitted, total_paid, total_delayed]
+    }]
+  }
 });
-}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,10 @@ import { loadDynamicBannerText } from '../components/banner';
 import { initNotificationBar } from "../components/notificationBar";
 import { changeColumn } from '../components/changeColumn';
 import { initFacture } from '../components/facture';
-// import {formValidation} from "../components/formValidation";
-import {initFullCalendar} from '../plugins/fullcalendar';
+import {formValidation} from "../components/formValidation";
+// import {initFullCalendar} from '../plugins/fullcalendar';
 import { turnoverChart } from '../components/analyticsCharts';
-import { gaugeChart } from '../components/gaugeChart';
+// import { gaugeChart } from '../components/gaugeChart';
 import { donutChartbis } from '../components/donutChartbis';
 
 
@@ -20,47 +20,10 @@ if (document.querySelector('#new_invoice')) {
 
 if (document.querySelector(".notification-bar")) {
     initNotificationBar();
-    initFullCalendar();
+
 }
 
 
-
-// const formValidation = () => {
-//   const steps = [...document.querySelectorAll('a.step')];
-//   const tabButtonsDivs = document.querySelectorAll('.tab-buttons');
-//   let buttons = [];
-//   tabButtonsDivs.forEach(div => buttons.push([...div.querySelectorAll('button')]));
-//   buttons = buttons.flat();
-//   buttons.forEach(button => button.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     const activeStep = document.querySelector('a.active');
-//     const index = steps.findIndex(step => step === activeStep);
-//     const nextStep = steps[index + 1];
-//     const previousStep = steps[index - 1];
-//     const parentDiv = event.target.parentElement.parentElement;
-//     const formInputs = parentDiv.querySelectorAll('input');
-//     // console.log(formInputs);
-//     if (event.target.classList.contains('next-btn')) {
-//     // if (event.target.classList.contains('next-btn') && checkForm(formInputs)) {
-//       nextStep.click();
-//     // } else if (checkForm(formInputs)) {
-//     //   previousStep.click();
-//     } else {
-//       // displayErrors();
-//       previousStep.click();
-//     }
-//   }));
-// }
-// const checkForm = (formInputs) => {
-//   console.log(formInputs);
-//   [...formInputs].every(input => input.value);
-// }
-// const displayErrors = () => {
-//   console.log("Form isn't completed");
-// }
-
-
-// // loadDynamicBannerText();
 
 //formValidation();
 
