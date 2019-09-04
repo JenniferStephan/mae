f3868772262dbd957521444ce21fc0c6acce85e0
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-09-04 21:42:06 +0200

    404 et config

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Mae
 
     config.i18n.default_locale = :fr
 
+    config.active_job.queue_adapter = :sidekiq
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  require "sidekiq/web"
+  authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   resources :simulations
   get "/discover", to: "pages#discover", as: "landing_page"
 
