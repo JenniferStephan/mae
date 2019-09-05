ffe6e9b0f1e3265a3a9800eaa62144e125afc64f
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-09-05 10:51:52 +0200

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
-class UsersController < ApplicationController  before_action :set_user, only: [:edit, :update]
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit, :update]
 
   def edit
   end
 
   def update
   end
-  
+
   def create
   end
 
@@ -24,5 +26,5 @@ class UsersController < ApplicationController  before_action :set_user, only: [:
   def user_params
     params.require(:invoice).permit(:first_name, :last_name, :email, :username, :siret, :tax_rate, :address, :vat)
   end
-  
+
 end
